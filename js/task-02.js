0f214ee9d44c7026cf55cca77af3e9d6859d061a
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-02-07 00:52:33 +0200

    Добавляє початкове рішення 2завдання

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,21 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = document.querySelector('#ingredients');
+// console.log(ingredientsEl);
+
+const itemEl = document.createElement('li');
+console.log(itemEl);
+
+ingredients.forEach(ingredient => {
+  console.log(ingredient);
+  itemEl.textContent = `${ingredient}`;
+  ingredientsEl.append(itemEl);
+}
+)
+
+
+
+  
+
