9faea976e1105508413cf0bfa5710facba4a8808
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-02-13 23:15:46 +0200

    Перевіряє наявність класу і видаляє його

--- js/task-06.js
+++ js/task-06.js
@@ -12,20 +12,42 @@
 
 // Для додавання стилів використовуй CSS-класи valid і invalid, які ми вже додали у вихідні файли завдання.
 
-const inputEl = document.querySelector('#validation-input');
+let inputEl = document.querySelector('#validation-input');
 
 // значення атрибута data-length
-// console.log(inputEl.dataset.length); 
+// console.log(inputEl.dataset.length);
 
+// const onInputElBlur = (event) => {
+//     // довжина введених символів в інпут
+//     // console.log(event.currentTarget.value.length);
+
+//     // inputEl.classList = [];
+//     if (event.currentTarget.value.length == inputEl.dataset.length) {
+//         inputEl.classList.add('valid');
+//         console.log(inputEl);
+//     } else {
+//         inputEl.classList.add('invalid');
+//         console.log(inputEl);
+//     }
+// }
+
+
+// рішення з перевіркою наявності класу і його видалення
 const onInputElBlur = (event) => {
-    // довжина введених символів в інпут
-    // console.log(event.currentTarget.value.length);
-    if (event.currentTarget.value.length == inputEl.dataset.length) {
+
+       if (event.currentTarget.value.length == inputEl.dataset.length) {
         inputEl.classList.add('valid');
+         if (inputEl.classList.contains('invalid')) {
+            inputEl.classList.remove('invalid');
+        }
     } else {
         inputEl.classList.add('invalid');
+        if (inputEl.classList.contains('valid')) {
+            inputEl.classList.remove('valid');
+        }
     }
 }
 
 inputEl.addEventListener('blur', onInputElBlur);
+
 
